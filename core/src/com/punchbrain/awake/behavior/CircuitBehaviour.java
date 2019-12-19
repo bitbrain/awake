97c9a45c7a253ae209173a57be02cfe71b1cc1f9
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-12-19 11:28:10 -0600

    ":recycle: refactor circuit behaviour"

--- core/src/com/punchbrain/awake/behavior/CircuitBehaviour.java
+++ core/src/com/punchbrain/awake/behavior/CircuitBehaviour.java
@@ -1,9 +1,5 @@
 package com.punchbrain.awake.behavior;
 
-import box2dLight.Light;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.math.Rectangle;
-import com.punchbrain.awake.animation.LampState;
 import com.punchbrain.awake.model.Circuit;
 import de.bitbrain.braingdx.behavior.BehaviorAdapter;
 import de.bitbrain.braingdx.context.GameContext2D;
@@ -17,15 +13,10 @@ import static com.punchbrain.awake.GameObjectType.PLAYER;
 public class CircuitBehaviour extends BehaviorAdapter {
 
     private Circuit circuit;
-    private GameContext2D context;
-    private Light lampLight;
-    private boolean continuous = false;
-    private float deltaAccumulator = 0;
 
 
     public CircuitBehaviour(Circuit circuit, GameContext2D context){
         this.circuit = circuit;
-        this.context = context;
     }
 
     @Override
@@ -36,11 +27,7 @@ public class CircuitBehaviour extends BehaviorAdapter {
     @Override
     public void update(GameObject source, GameObject player, float delta) {
         if(PLAYER.isTypeOf(player)){
-            Rectangle playerRect = new Rectangle();
-            playerRect.set(player.getLeft(), player.getTop(), player.getWidth(), player.getHeight());
-            Rectangle circuitRect = new Rectangle();
-            circuitRect.set(source.getLeft(), source.getTop(), source.getWidth(), source.getHeight());
-            if(playerRect.contains(circuitRect) || playerRect.overlaps(circuitRect)){
+            if(player.collidesWith(source)){
                 circuit.resolvePlayerCollision();
             }
         }
